e37ab634a10f66dc7db90b9d50cdfe6c0da5c5f8
Khrystyna979 <khrystynaa.ol@gmail.com>
2026-06-24 12:01:21 +0300

    fix: запобігання дублікатам записів у  MongoDB

--- scrapy_scraper/quote_spyder/seed.py
+++ scrapy_scraper/quote_spyder/seed.py
@@ -8,22 +8,23 @@ def load_data():
     with open(authors_file, 'r', encoding='utf-8') as af:
         data = json.load(af)
         for item in data:
-            author = Author(**item) # unpack the dictionary into an object
-            author.save()
+            if not Author.objects(fullname=item['fullname']):
+                author = Author(**item)
+                author.save()
 
     with open(quotes_file, 'r', encoding='utf-8') as qf:
         data = json.load(qf)
         for item in data:
             author_fullname = item.get('author')
-            author = Author.objects(fullname=author_fullname).first() # searching for the author in the database
-
+            author = Author.objects(fullname=author_fullname).first()
             if author:
-                quote = Quote(
+                if not Quote.objects(quote=item.get('quote')):
+                    quote = Quote(
                         tags=item.get('tags'),
                         quote=item.get('quote'),
                         author=author
                     )
-                quote.save()
+                    quote.save()
 
 if __name__ == '__main__':
     load_data()
